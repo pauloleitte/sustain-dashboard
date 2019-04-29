d5a846946c3330a2f9d7ca50dd83a550b3686003
pauloleitte <paulo.leitte@live.com>
2019-04-29 15:30:31 -0300

    Ajuste nas rotas

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { AutomovelComponent } from "./automovel/automovel.component";
-import { MassificadosComponent } from "./massificados/massificados.component";
-import { ChartBarsComponent } from "./chart-bars/chart-bars.component";
 import { ListComponent } from "./list/list.component";
 import { ChamadoComponent } from "./chamado/chamado.component";
 
 
 const rotas: Routes = [
-  { path: "dashboard", component: DashboardComponent },
-  //{ path: "automovel", component: AutomovelComponent },
-  //{ path: "massificado", component: MassificadosComponent },
-  //{ path: "chart", component: ChartBarsComponent },
+  {path: "dashboard", component: DashboardComponent },
   {path: "chamado", component: ChamadoComponent},
   {path: "lista", component: ListComponent },
   {path: "", component: DashboardComponent },
